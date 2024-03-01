fce754cc0d43a5c15f7f8a0f423d95503f512132
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-03-01 13:03:25 -0600

    feat: finish factory fn to create new ship object

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 export function shipFactory(length) {
-  if (!(typeof length === "number")) {
+  if (typeof length !== "number") {
     return null;
   }
 
   const ship = Array(length);
+
   for (let i = 0; i < length; i++) {
     ship[i] = 0;
   }
 
-  return { ship };
+  const isSunk = () => {
+    const sumOfArray = ship.reduce(
+      (accumulator, newNumber) => accumulator + newNumber,
+      0
+    );
+    return sumOfArray === ship.length;
+  };
+
+  const hit = (index) => {
+    ship[index] = 1;
+  };
+
+  return { ship, hit, isSunk };
 }
--- script.test.js
+++ script.test.js
@@ -1,13 +1,38 @@
 import { shipFactory } from "./script.js";
 
-test("check that ship is an array", () => {
-  expect(Array.isArray(shipFactory(3).ship)).toBeTruthy();
-});
-
-test("returns 'null' if length isNaN", () => {
-  expect(shipFactory("a")).toBeNull();
-});
-
-test("check ship array length", () => {
-  expect(shipFactory(4).ship.length).toEqual(4);
-});
+describe("test shipFactory()", () => {
+  test("check that ship is an array", () => {
+    expect(Array.isArray(shipFactory(3).ship)).toBeTruthy();
+  });
+  
+  test("returns 'null' if length isNaN", () => {
+    expect(shipFactory("2")).toBeNull();
+  });
+  
+  test("ship length 3 hit at index 0 is [ 1, 0, 0 ]", () => {
+    const newShip = shipFactory(3);
+    newShip.hit(0);
+    expect(newShip.ship.toString()).toBe("1,0,0");
+  });
+  
+  test("hits on the whole ship return array of all 1", () => {
+    const newShip = shipFactory(2);
+    newShip.hit(0);
+    newShip.hit(1);
+    expect(newShip.ship.toString()).toBe("1,1");
+  });
+  
+  test("hits on the whole ship sink it", () => {
+    const newShip = shipFactory(2);
+    newShip.hit(0);
+    newShip.hit(1);
+    expect(newShip.isSunk()).toBeTruthy();
+  });
+  
+  test("multiple hits on the same spot won't sink the ship", () => {
+    const newShip = shipFactory(2);
+    newShip.hit(0);
+    newShip.hit(0);
+    expect(newShip.isSunk()).toBeFalsy();
+  });
+})
