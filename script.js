06513e1b8703299a74655ffcd188b6096b0a637b
aRav3n <24660772+aRav3n@users.noreply.github.com>
2024-03-01 14:30:48 -0600

    feat: begin work on gameboardFactory()

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+export function getRandomIntLessThan(number) {
+  return Math.floor(Math.random() * number);
+}
+
 export function shipFactory(length) {
   if (typeof length !== "number") {
     return null;
@@ -22,4 +26,44 @@ export function shipFactory(length) {
   };
 
   return { ship, hit, isSunk };
+}
+
+export function gameboardFactory() {
+  const ships = [];
+  const gridSize = 10;
+
+  const checkIfValidPlacement = (length, x, y, orientation) => {
+    for (let i = 0; i < ships.length; i++) {}
+  };
+
+  const placeShip = (length, x, y, orientation) => {
+    if (
+      x > 10 ||
+      x < 0 ||
+      y > 10 ||
+      y < 0 ||
+      (x + 1) % 1 !== 0 ||
+      (y + 1) % 1 !== 0
+    ) {
+      throw new Error(`${x}, ${y} is not a valid set of coordinates`);
+    }
+
+    let xToUse = x;
+    let yToUse = y;
+    const ship = shipFactory(length);
+    const shipLocation = Array(length);
+    if (orientation !== "h" && orientation !== "v") {
+      throw new Error("not a valid orientation, can only be 'h' or 'v'");
+    } else if (orientation === "h") {
+      if (xToUse > gridSize - length) {
+        xToUse = gridSize - length;
+      }
+      for (let i = 0; i < length; i++) {
+        ship[i][0] = xToUse + i;
+        ship[i][1] = yToUse;
+      }
+    }
+  };
+
+  return { ships, placeShip };
 }
--- script.test.js
+++ script.test.js
@@ -1,38 +1,62 @@
-import { shipFactory } from "./script.js";
+import {
+  gameboardFactory,
+  getRandomIntLessThan,
+  shipFactory,
+} from "./script.js";
+
+test("check random int function", () => {
+  expect(getRandomIntLessThan(1)).toEqual(0);
+});
 
 describe("test shipFactory()", () => {
   test("check that ship is an array", () => {
     expect(Array.isArray(shipFactory(3).ship)).toBeTruthy();
   });
-  
+
   test("returns 'null' if length isNaN", () => {
     expect(shipFactory("2")).toBeNull();
   });
-  
+
   test("ship length 3 hit at index 0 is [ 1, 0, 0 ]", () => {
     const newShip = shipFactory(3);
     newShip.hit(0);
     expect(newShip.ship.toString()).toBe("1,0,0");
   });
-  
+
   test("hits on the whole ship return array of all 1", () => {
     const newShip = shipFactory(2);
     newShip.hit(0);
     newShip.hit(1);
     expect(newShip.ship.toString()).toBe("1,1");
   });
-  
+
   test("hits on the whole ship sink it", () => {
     const newShip = shipFactory(2);
     newShip.hit(0);
     newShip.hit(1);
     expect(newShip.isSunk()).toBeTruthy();
   });
-  
+
   test("multiple hits on the same spot won't sink the ship", () => {
     const newShip = shipFactory(2);
     newShip.hit(0);
     newShip.hit(0);
     expect(newShip.isSunk()).toBeFalsy();
   });
-})
+});
+
+describe("test gameboardFactory()", () => {
+  test("placeShip only accepts 'h' or 'v' for orientation, else it throws the specified error", () => {
+    const newGameBoard = gameboardFactory();
+    expect(() => {
+      newGameBoard.placeShip(3, 4, 4, 7);
+    }).toThrow("not a valid orientation, can only be 'h' or 'v'");
+  });
+
+  test("placeShip outside of the grid throws the specified error", () => {
+    const newGameBoard = gameboardFactory();
+    expect(() => {
+      newGameBoard.placeShip(3, 4, 14, "h");
+    }).toThrow("4, 14 is not a valid set of coordinates");
+  });
+});
